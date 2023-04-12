fe7c3ab7c0d31beba9c32bc1a19b796089be4e8b
Jacob Park <jacobypark122@gmail.com>
2023-04-12 16:13:51 -0400

    done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db
+from models import db, Apartment, Tenant, Lease
 
 app = Flask( __name__ )
 app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///apartments.db'
@@ -11,6 +11,150 @@ app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS' ] = False
 migrate = Migrate( app, db )
 db.init_app( app )
 
+api = Api(app)
+
+class Apartments(Resource):
+    def get(self):
+        apartments = Apartment.query.all()
+        apartment_list = [apartment.to_dict() for apartment in apartments]
+
+        return make_response(apartment_list, 200)
+    
+    def post(self):
+        data = request.get_json()
+
+        new_apartment = Apartment(
+            number=data['number']
+        )
+        if new_apartment:
+            db.session.add(new_apartment)
+            db.session.commit()
+            return make_response(new_apartment.to_dict(), 200)
+        else:
+            return make_response({"error":"new apartment invalid data"}, 404)
+        
+api.add_resource(Apartments, "/apartments")
+
+class ApartmentsByID(Resource):
+    def get(self,id):
+        apartment = Apartment.query.filter_by(id=id).first().to_dict()
+        return make_response(apartment, 200)
+    
+    def patch(self,id):
+        data = request.get_json()
+
+        apartment = Apartment.query.filter_by(id=id).first()
+
+        for attr in data:
+            setattr(apartment, attr, data[attr])
+
+        db.session.add(apartment)
+        db.session.commit()
+        
+        return make_response(apartment.to_dict(), 200)
+    
+    def delete(self, id):
+        apartment = Apartment.query.filter_by(id=id).first()
+
+        db.session.delete(apartment)
+        db.session.commit()
+
+        return make_response({},204)
+
+api.add_resource(ApartmentsByID, '/apartments/<int:id>')
+
+class Tenants(Resource):
+    def get(self):
+        tenants = Tenant.query.all()
+        tenant_list = [tenant.to_dict() for tenant in tenants]
+
+        return make_response(tenant_list, 200)
+    
+    def post(self):
+        data = request.get_json()
+
+        new_tenant = Tenant(
+            name=data['name'],
+            age=data['age']
+        )
+        if new_tenant:
+            db.session.add(new_tenant)
+            db.session.commit()
+            return make_response(new_tenant.to_dict(), 200)
+        else:
+            return make_response({"error":"new tenant invalid data"}, 404)
+        
+api.add_resource(Tenants, "/tenants")
+
+class TenantsByID(Resource):
+    def get(self,id):
+        tenant = Tenant.query.filter_by(id=id).first().to_dict()
+        return make_response(tenant, 200)
+    
+    def patch(self,id):
+        data = request.get_json()
+
+        tenant = Tenant.query.filter_by(id=id).first()
+
+        for attr in data:
+            setattr(tenant, attr, data[attr])
+
+        db.session.add(tenant)
+        db.session.commit()
+        
+        return make_response(tenant.to_dict(), 200)
+    
+    def delete(self, id):
+        tenant = Tenant.query.filter_by(id=id).first()
+
+        db.session.delete(tenant)
+        db.session.commit()
+
+        return make_response({},204)
+
+api.add_resource(TenantsByID, '/tenants/<int:id>')
+
+class Leases(Resource):
+    def get(self):
+        leases = Tenant.query.all()
+        lease_list = [lease.to_dict() for lease in leases]
+
+        return make_response(lease_list, 200)
+    def post(self):
+        data = request.get_json()
+
+        new_lease = Tenant(
+            rent=data['rent']
+        )
+        if new_lease:
+            db.session.add(new_lease)
+            db.session.commit()
+            return make_response(new_lease.to_dict(), 200)
+        else:
+            return make_response({"error":"new lease invalid data"}, 404)
+
+api.add_resource(Leases, "/leases")
+
+class LeasesByID(Resource):
+    def get(self, id):
+        lease = Lease.query.filter_by(id=id).first().to_dict()
+        if lease:
+            return make_response(lease, 200)
+        else:
+            return make_response({"error":"invalid id"}, 404)
+        
+    def delete(self, id):
+        lease = Lease.query.filter_by(id=id).first()
+        if lease:
+            db.session.delete(lease)
+            db.session.commit()
+            return make_response({}, 204)
+        else:
+            return make_response({"error":"invalid id"}, 404)
+        
+api.add_resource(LeasesByID, '/leases/<int:id>')
+
+        
 
 if __name__ == '__main__':
     app.run( port = 3000, debug = True )
--- server/models.py
+++ server/models.py
@@ -5,3 +5,65 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
+
+class Apartment(db.Model, SerializerMixin):
+    __tablename__ = "apartments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    
+    leases = db.relationship('Lease', back_populates = 'apartment')
+    tenants = association_proxy('leases', 'apartment')
+
+    serialize_rules = ('-created_at','-updated_at','-tenants.created_at', '-tenants.updated_at','-tenants.apartments','-leases')
+
+
+class Tenant(db.Model, SerializerMixin):
+    __tablename__ = "tenants"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    
+    leases = db.relationship('Lease', back_populates = 'tenant')
+    apartments = association_proxy('leases', 'tenant')
+
+    serialize_rules = ('-created_at','-updated_at','-apartments.created_at', '-apartments.updated_at','-apartments.tenants','-leases')
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if len(name) < 1:
+            raise ValueError("Name must be present")
+        return name
+    
+    @validates("age")
+    def validate_age(self, key, age):
+        if age < 18:
+            raise ValueError("Tenant too young")
+        return age
+    
+
+class Lease(db.Model, SerializerMixin):
+    __tablename__ = 'leases'
+
+    id = db.Column(db.Integer, primary_key = True)
+    rent = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    apartment_id = db.Column(db.Integer, db.ForeignKey('apartments.id'))
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'))
+    
+    apartment = db.relationship('Apartment', back_populates = 'leases')
+    tenant = db.relationship('Tenant', back_populates = 'leases')
+
+    serialize_rules = ('-created_at', '-updated_at', '-apartment.leases', '-tenant.leases', '-apartment.tenants', '-tenant.apartments')
+    
+
+
